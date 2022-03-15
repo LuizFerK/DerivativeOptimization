1f3557e9dc75956369df1647d7afc4e3986c9004
LuizFerK <luizfernandoklein@live.com>
2022-03-14 21:04:53 -0300

    feat: Can problem solved

--- can.c
+++ can.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  printf("Hello world\n");
+  // Declaração de variáveis
+  float v, x, y, h, r, al, ab, at, p;
+
+  // Pegar as informações necessárias
+  printf("Por favor, digite as informações necessárias.\n");
+
+  // v = volume
+  printf("Volume da lata (cm³): ");
+  scanf("%f", &v);
+
+  // x = preço do material da base da lata
+  printf("Preço do material da base da lata (por m²): ");
+  scanf("%f", &x);
+
+  // y = preço do material da lateral da lata
+  printf("Preço do material da lateral da lata (por m²): ");
+  scanf("%f", &y);
+
+  // r = raio da lata; cbrt = raiz cúbica; M_PI = π
+  r = cbrt((v * y) / (2 * x * M_PI));
+
+  // h = altura da lata; pow = potenciação
+  h = v / (M_PI * pow(r, 2));
+
+  // al = área lateral
+  al = h * 2 * M_PI * r;
+
+  // ab = área da base
+  ab = M_PI * pow(r, 2);
+
+  // at = área total
+  at = al + ab * 2;
+
+  // p = preço total da lata
+  p = al * y + ab * 2 * x;
+
+  // Saídas
+  printf("\nRaio da base da lata: %.2f\n", r);
+  printf("Altura da lata: %.2f\n", h);
+  printf("Área total da superficie da lata: %.2f\n", at);
+  printf("Custo total do material usado na lata: %.2f\n", p);
   
   return 0;
 }
--- turism.c
+++ turism.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  printf("Hello world\n");
+  // Declaração de variáveis
+  float x, y, e, d;
+
+  // Pegar as informações necessárias
+  printf("Por favor, digite as informações necessárias.\n");
+
+  // x = valocidade da barca
+  printf("Velocidade da barca (km/h): ");
+  scanf("%f", &x);
+
+  // y = valocidade do carro
+  printf("Velocidade do carro (km/h): ");
+  scanf("%f", &y);
+
+  // e = distância da estação até a cidade
+  // e =
+
+  // d = distância da ilha até a cidade
+  // d = 
+
+  // Saídas
+  printf("\nDistância da estação até a cidade: %.2f\n", e);
+  printf("Distância da ilha até a cidade : %.2f\n", d);
   
   return 0;
 }
